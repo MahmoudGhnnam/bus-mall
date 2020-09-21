989ee244e0d11cc82e4abd469ff17555799d49e7
MahmoudGhnnam <mahmou999@gmail.com>
2020-09-21 06:50:03 +0300

    add files

---
+++ app.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bus-mall</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="/img/bus.png" alt="bus">
+     <h1>Bus Mall</h1>
+    
+    </header>
+    
+    <main>
+     <section id="images-section">
+     <img id="left-img"  alt="" title="">
+     <img id="mid-img"  alt="" title="">
+     <img id="right-img"  alt="" title="">
+     </section>
+    </main>
+
+    <footer>
+
+    </footer>
+    <script src="app.js"></script>
+</body>
+</html>
